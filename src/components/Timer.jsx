63c495cd0685c845ff0a02646b02da17c00a405c
Alejandro Martinez <Alejandro.MartinezdelaHaba@Aareon.com>
2021-06-01 18:19:11 +0200

    + Added timer component

---
+++ src/components/Timer.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+export default function Timer() {
+  let timerStyle = {
+    color: "#616161",
+  };
+
+  let [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  }, []);
+  return (
+    <div>
+      <p style={timerStyle} className="timer">
+        {" "}
+        {date.toLocaleTimeString()}
+      </p>
+    </div>
+  );
+}
