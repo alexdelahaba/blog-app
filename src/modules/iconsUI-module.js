649f16046ae9aa37a8125763b0927401f33dd22e
Alejandro Martinez <Alejandro.MartinezdelaHaba@Aareon.com>
2021-06-07 14:58:48 +0200

    + Added module for material ui icons

--- src/modules/iconsUI-module.js
+++ src/modules/iconsUI-module.js
@@ -1,3 +1,21 @@
 import BookIcon from "@material-ui/icons/Book";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import CloseIcon from "@material-ui/icons/Close";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import HomeIcon from "@material-ui/icons/Home";
+import MenuIcon from "@material-ui/icons/Menu";
+import PeopleIcon from "@material-ui/icons/People";
 
-export { BookIcon };
+export {
+  BookIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  CloseIcon,
+  ExpandLessIcon,
+  ExpandMoreIcon,
+  HomeIcon,
+  MenuIcon,
+  PeopleIcon,
+};
